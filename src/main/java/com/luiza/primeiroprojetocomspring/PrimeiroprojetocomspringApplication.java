a756f3e133f90ca53b9d64db6336fdbf66e083bf
LuizaProcopio <luiza.tavares.mp@gmail.com>
2026-08-02 18:05:15 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/luiza/primeiroprojetocomspring/PrimeiroprojetocomspringApplication.java
@@ -0,0 +1,13 @@
+package com.luiza.primeiroprojetocomspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroprojetocomspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroprojetocomspringApplication.class, args);
+	}
+
+}
