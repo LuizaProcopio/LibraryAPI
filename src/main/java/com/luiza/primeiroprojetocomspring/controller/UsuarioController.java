b55e2119a5d00d571f94255d2b26cd2583b0f1be
LuizaProcopio <luiza.tavares.mp@gmail.com>
2026-08-11 20:21:11 -0300

    Criação inicial do Usuario Controller

---
+++ src/main/java/com/luiza/primeiroprojetocomspring/controller/UsuarioController.java
@@ -0,0 +1,55 @@
+package com.luiza.primeiroprojetocomspring.controller;
+
+import com.luiza.primeiroprojetocomspring.database.model.UsuarioEntity;
+import com.luiza.primeiroprojetocomspring.database.repository.IUsuarioRepository;
+import com.luiza.primeiroprojetocomspring.dto.UsuarioDto;
+import com.luiza.primeiroprojetocomspring.service.UsuarioService;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.ConcreteProxy;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UsuarioDto findById(@PathVariable UUID id){
+        return usuarioService.findById(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<UsuarioEntity> findAll(){
+        return usuarioService.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addUser(@RequestBody UsuarioDto usuarioDto){
+        usuarioService.save(usuarioDto);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable UUID id){
+        usuarioService.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UsuarioDto updateById(@PathVariable UUID id, @RequestBody UsuarioDto usuarioDto){
+        return usuarioService.updateById(id, usuarioDto);
+    }
+}
