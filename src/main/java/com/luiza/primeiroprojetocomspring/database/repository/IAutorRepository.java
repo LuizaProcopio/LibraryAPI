eeea7f159b73f57f221a92d7ef58dfeb01c0c068
LuizaProcopio <luiza.tavares.mp@gmail.com>
2026-08-22 12:24:35 -0300

    Modificação do tipo do ID no AutorRepository

--- src/main/java/com/luiza/primeiroprojetocomspring/database/repository/IAutorRepository.java
+++ src/main/java/com/luiza/primeiroprojetocomspring/database/repository/IAutorRepository.java
@@ -3,7 +3,6 @@ package com.luiza.primeiroprojetocomspring.database.repository;
 import com.luiza.primeiroprojetocomspring.database.model.AutorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
 
-public interface IAutorRepository extends JpaRepository<AutorEntity, UUID> {
+public interface IAutorRepository extends JpaRepository<AutorEntity, Integer> {
 }
