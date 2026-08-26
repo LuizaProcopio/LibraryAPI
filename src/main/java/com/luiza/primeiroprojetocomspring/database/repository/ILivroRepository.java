f19d4df0a9c35dd7614e664da32419ac2fa09e39
LuizaProcopio <luiza.tavares.mp@gmail.com>
2026-08-25 23:11:42 -0300

    Modificação do tipo do ID no LivroRepository

--- src/main/java/com/luiza/primeiroprojetocomspring/database/repository/ILivroRepository.java
+++ src/main/java/com/luiza/primeiroprojetocomspring/database/repository/ILivroRepository.java
@@ -3,7 +3,6 @@ package com.luiza.primeiroprojetocomspring.database.repository;
 import com.luiza.primeiroprojetocomspring.database.model.LivroEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
 
-public interface ILivroRepository extends JpaRepository<LivroEntity, UUID> {
+public interface ILivroRepository extends JpaRepository<LivroEntity, Integer> {
 }
