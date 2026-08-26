9ddcdd81e3099922ce518f4f326171bc4e4bf6fc
LuizaProcopio <luiza.tavares.mp@gmail.com>
2026-08-25 23:10:45 -0300

    Modificação do tipo do ID no EmprestimoRepository

--- src/main/java/com/luiza/primeiroprojetocomspring/database/repository/IEmprestimoRepository.java
+++ src/main/java/com/luiza/primeiroprojetocomspring/database/repository/IEmprestimoRepository.java
@@ -3,7 +3,6 @@ package com.luiza.primeiroprojetocomspring.database.repository;
 import com.luiza.primeiroprojetocomspring.database.model.EmprestimoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
 
-public interface IEmprestimoRepository extends JpaRepository<EmprestimoEntity, UUID> {
+public interface IEmprestimoRepository extends JpaRepository<EmprestimoEntity, Integer> {
 }
