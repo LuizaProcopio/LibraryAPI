e43a4fc68ba8b23f500049ad8dce732a4a1d63b1
LuizaProcopio <luiza.tavares.mp@gmail.com>
2026-08-30 14:11:07 -0300

    Modificação do Usuario Service para incluir IDs e relacionar os empréstimos do usuário

--- src/main/java/com/luiza/primeiroprojetocomspring/service/UsuarioService.java
+++ src/main/java/com/luiza/primeiroprojetocomspring/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.luiza.primeiroprojetocomspring.service;
 
+import com.luiza.primeiroprojetocomspring.database.model.EmprestimoEntity;
 import com.luiza.primeiroprojetocomspring.database.model.UsuarioEntity;
 import com.luiza.primeiroprojetocomspring.database.repository.IUsuarioRepository;
 import com.luiza.primeiroprojetocomspring.dto.UsuarioDto;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,20 +21,38 @@ public class UsuarioService {
         UsuarioEntity usuario = usuarioRepository.findById(id).orElseThrow();
 
         return UsuarioDto.builder()
+                .id(usuario.getId())
                 .nome(usuario.getNome())
                 .email(usuario.getEmail())
+                .emprestimos(usuario.getEmprestimos().stream()
+                        .map(EmprestimoEntity::getId)
+                        .collect(Collectors.toSet()))
                 .build();
     }
 
-    public List<UsuarioEntity> findAll(){
-        return usuarioRepository.findAll();
+    public List<UsuarioDto> findAll(){
+        return usuarioRepository.findAll().stream()
+                .map(usuario -> UsuarioDto.builder()
+                        .id(usuario.getId())
+                        .nome(usuario.getNome())
+                        .email(usuario.getEmail())
+                        .emprestimos(usuario.getEmprestimos().stream()
+                                .map(EmprestimoEntity::getId)
+                                .collect(Collectors.toSet())).build()).toList();
 
     }
 
-    public void save(UsuarioDto usuarioDto){
-        usuarioRepository.save(UsuarioEntity.builder()
+    public UsuarioDto save(UsuarioDto usuarioDto){
+        UsuarioEntity usuario = usuarioRepository.save(UsuarioEntity.builder()
                 .nome(usuarioDto.getNome())
-                .email(usuarioDto.getEmail()).build());
+                .email(usuarioDto.getEmail())
+                .build());
+
+        return UsuarioDto.builder()
+                .id(usuario.getId())
+                .nome(usuario.getNome())
+                .email(usuario.getEmail())
+                .build();
     }
 
     public void deleteById(UUID id){
@@ -48,6 +68,7 @@ public class UsuarioService {
         usuarioRepository.save(usuario);
 
         return UsuarioDto.builder()
+                .id(usuario.getId())
                 .nome(usuario.getNome())
                 .email(usuario.getEmail()).build();
     }
